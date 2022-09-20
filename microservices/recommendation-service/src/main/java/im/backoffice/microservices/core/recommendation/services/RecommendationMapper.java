9f46ea0c2fa625f4a0cbdfd90352f1ae82355410
rick <rickjones56@msn.com>
2022-09-20 17:24:56 -0600

    Finally got all db and message queue connections working in docker-compose

---
+++ microservices/recommendation-service/src/main/java/im/backoffice/microservices/core/recommendation/services/RecommendationMapper.java
@@ -0,0 +1,29 @@
+package im.backoffice.microservices.core.recommendation.services;
+
+import im.backoffice.microservices.core.recommendation.persistence.RecommendationEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import im.backoffice.api.core.recommendation.Recommendation;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface RecommendationMapper {
+
+    @Mappings({
+        @Mapping(target = "rate", source="entity.rating"),
+        @Mapping(target = "serviceAddress", ignore = true)
+    })
+    Recommendation entityToApi(RecommendationEntity entity);
+
+    @Mappings({
+        @Mapping(target = "rating", source="api.rate"),
+        @Mapping(target = "id", ignore = true),
+        @Mapping(target = "version", ignore = true)
+    })
+    RecommendationEntity apiToEntity(Recommendation api);
+
+    List<Recommendation> entityListToApiList(List<RecommendationEntity> entity);
+    List<RecommendationEntity> apiListToEntityList(List<Recommendation> api);
+}
